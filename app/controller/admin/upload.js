2a889019fd170073c79730171825d6e8dd2805e8
linjinghui <852530363@qq.com>
2019-06-19 08:53:24 +0800

    fix: 上传图片路径问题

--- app/controller/admin/upload.js
+++ app/controller/admin/upload.js
@@ -45,8 +45,7 @@ class UploadController extends Controller {
 
         remoteFileStream.on('finish', async () => {
           if (errFlag) return;
-          // resBody = util.resdata(200, 'http://' + ctx.request.header.host + filePath);
-          resBody = util.resdata(200, ctx.request.header.origin + '/api' + filePath);
+          resBody = util.resdata(200, ctx.request.header.referer + 'api' + filePath);
           resolve({ fileName, url: ctx.request.header.host + filePath });
         });
       });
